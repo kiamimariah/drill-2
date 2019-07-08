9d969d8a40346f4ef776db0644abc056c747ed61
Kiami Nichols <kiamimariah@yahoo.com>
2019-07-08 16:41:52 -0400

    buzzlite

--- jedi.js
+++ jedi.js
@@ -2,3 +2,25 @@ function jediName(firstName,lastName) {
     return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
 console.log(jediName('Kiami','Mariah'));
+
+function beyond(num) {
+    if(num === Infinity || num === Infinity){
+    console.log('And beyond!');
+}
+     if (num < 0) {
+    console.log('To negative infinity');
+}
+     if (num > 0) {
+    console.log('to infinity');
+}
+    else {
+        console.log('Staying home');
+    }
+}
+
+beyond(5);
+beyond(7);
+beyond(100);
+beyond(0);
+beyond(-33);
+
